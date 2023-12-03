9a4c90f759179b1fe0f83aa7067e37e8857fa1ae
cantoniac <cboldis@yahoo.com>
2023-12-03 23:30:53 +0200

    pypika

--- pythonProject/db_connector.py
+++ pythonProject/db_connector.py
@@ -1,7 +1,9 @@
+import pydoc
 from datetime import datetime
 
 import pymysql
 from pymysql import IntegrityError
+from pypika import Table, Query, Database
 
 schema_name = 'mydb'
 
@@ -12,12 +14,14 @@ def insert(name, user_id):
     cursor = conn.cursor()
 
     try:
-        cursor.execute(f"INSERT into mydb.users (id, name, creation_date) VALUES ('{user_id}', '{name}', '{str(datetime.now())}')")
+        cursor.execute(
+            f"INSERT into mydb.users (id, name, creation_date) VALUES ('{user_id}', '{name}', '{str(datetime.now())}')")
     except IntegrityError as e:
         cursor.execute("SELECT max(id) from users")
         nextid = cursor.fetchone()[0] + 1
 
-        cursor.execute(f"INSERT into mydb.users (id, name, creation_date) VALUES ('{nextid}', '{name}', '{str(datetime.now())}')")
+        cursor.execute(
+            f"INSERT into mydb.users (id, name, creation_date) VALUES ('{nextid}', '{name}', '{str(datetime.now())}')")
     finally:
         cursor.close()
         conn.close()
@@ -57,12 +61,24 @@ def delete(user_id):
 
 
 def get_user_name(user_id):
+    """
+        Params:
+          x and b are numbers (int or float)
+        Returns:
+          the sum of x and y.
+    """
     conn = pymysql.connect(host='127.0.0.1', port=3306, user='user', password='password', db=schema_name)
     conn.autocommit(True)
     cursor = conn.cursor()
 
     try:
-        cursor.execute(f"SELECT name FROM mydb.users WHERE id='{user_id}'")
+        users = Table('users')
+
+        q = Query.from_(users).select(users.name).where(users.id == int(user_id))
+
+        query = q.get_sql(quote_char=None)
+        cursor.execute(query)
+
         if cursor.rowcount == 0:
             return -1
         else:
@@ -75,5 +91,4 @@ def get_user_name(user_id):
     return name
 
 
-
-
+pydoc.writedoc('my_module')
