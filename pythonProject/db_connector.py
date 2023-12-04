461159f5d4e9791b0ef0b3b434fb7c69e0952434
cantoniac <cboldis@yahoo.com>
2023-12-04 12:17:52 +0200

    pypika * preparedStatement

--- pythonProject/db_connector.py
+++ pythonProject/db_connector.py
@@ -13,15 +13,16 @@ def insert(name, user_id):
     conn.autocommit(True)
     cursor = conn.cursor()
 
+
     try:
-        cursor.execute(
-            f"INSERT into mydb.users (id, name, creation_date) VALUES ('{user_id}', '{name}', '{str(datetime.now())}')")
+        prepstmt = "INSERT INTO users (id, name, creation_date) VALUES (%s, %s, %s)"
+        cursor.execute(prepstmt, (user_id, name, str(datetime.now())))
     except IntegrityError as e:
         cursor.execute("SELECT max(id) from users")
         nextid = cursor.fetchone()[0] + 1
 
-        cursor.execute(
-            f"INSERT into mydb.users (id, name, creation_date) VALUES ('{nextid}', '{name}', '{str(datetime.now())}')")
+        prepstmt = "INSERT INTO users (id, name, creation_date) VALUES (%s, %s, %s)"
+        cursor.execute(prepstmt, (nextid, name, str(datetime.now())))
     finally:
         cursor.close()
         conn.close()
@@ -34,7 +35,13 @@ def update(name, user_id):
     cursor = conn.cursor()
 
     try:
-        cursor.execute(f"UPDATE mydb.users SET name = '{name}' WHERE id='{user_id}'")
+        users = Table('users')
+
+        q = Query.update(users).set(users.name, name).where(users.id == int(user_id))
+
+        query = q.get_sql(quote_char=None)
+        cursor.execute(query)
+
     except IntegrityError as e:
         return -1
     finally:
@@ -49,7 +56,13 @@ def delete(user_id):
     cursor = conn.cursor()
 
     try:
-        cursor.execute(f"DELETE FROM mydb.users WHERE id='{user_id}'")
+        users = Table('users')
+
+        q = Query.from_(users).delete().where(users.id == int(user_id))
+
+        query = q.get_sql(quote_char=None)
+        cursor.execute(query)
+
         if cursor.rowcount == 0:
             return -1
     except IntegrityError as e:
@@ -91,4 +104,4 @@ def get_user_name(user_id):
     return name
 
 
-pydoc.writedoc('my_module')
+
